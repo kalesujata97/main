784d516104476dfff6a96b5c337f32b60ca92756
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-15 10:13:47 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,30 +1,17 @@
-pipeline {
-    agent any
-    
-    stages {
-		stage('Payment Testsuite Execution'){
-
-			steps{
-			checkout([$class: 'GitSCM',
-			  branches: [[name: '*/master']],
-			  userRemoteConfigs: [
-				[url: 'https://github.com/SujataKale97/new1.git',
-				 credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c']
-			  ]
-			])  
+node {
+	stage('Payment Testsuite Execution'){
+		checkout([$class: 'GitSCM',
+		branches: [[name: '*/master']],
+		userRemoteConfigs: [
+		[url: 'https://github.com/SujataKale97/new1.git',
+		credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c']
+		]])  
 			
-                        bat ' gradlew ' +
+                 bat ' gradlew ' +
                       
-                       "clean test -i -Ppayment"
+                 "clean test -i -Ppayment"
 	
-			 publishHTML([reportDir: 'test-output', reportFiles: 'PaymentServiceReport.html', reportName: 'Payment Test-suite Report'])
-			}
-		}
-	    
+		publishHTML([reportDir: 'test-output', reportFiles: 'PaymentServiceReport.html', reportName: 'Payment Test-suite Report'])
+		 emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'kale.babanrao@happiestminds.com'	
 	}
-	post {
-    always {
-        emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test', to: 'kale.babanrao@happiestminds.com'
-    	}
-    }
 }

18de237b2be35745599bd3f2be665e64f050fcc1
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-20 11:25:26 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -23,4 +23,15 @@ node {
             
 	     )
         }
+	stage('Sonarqube analysis') {
+    steps {
+    script {
+             scannerHome = tool 'SonarScanner';
+        }
+     withSonarQubeEnv('SonarQube') {
+         bat "${scannerHome}/bin/sonar-scanner.bat" 
+    }
+
+    }
+        }
 }

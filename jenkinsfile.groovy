724d93d802a56e4b70bb7c5bb8932ef59c7656e7
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-27 13:01:47 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,7 @@ node {
 		checkout([$class: 'GitSCM',
 		branches: [[name: '*/master']],
 		userRemoteConfigs: [
-		[url: 'https://github.com/SujataKale97/new1.git',
+		[url: 'https://github.com/SujataKale97/test.git',
 		credentialsId: 'ea4c3770-b2ed-4639-9ffc-cc3e586e454c']
 		]])  
 			

d635acf9b8a0c00e08903b48a05c24c4c9b4c2e3
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-28 12:19:59 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,25 @@
 import hudson.tasks.test.AbstractTestResultAction
 import hudson.tasks.junit.CaseResult
 
+def getTestSummary = { ->
+    def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+    def summary = ""
+
+    if (testResultAction != null) {
+        total = testResultAction.getTotalCount()
+        failed = testResultAction.getFailCount()
+        skipped = testResultAction.getSkipCount()
+
+        summary = "Passed: " + (total - failed - skipped)
+        summary = summary + (", Failed: " + failed)
+        summary = summary + (", Skipped: " + skipped)
+    } else {
+        summary = "No tests found"
+    }
+    return summary
+}
+
+
 node {
 	stage('Payment Testsuite Execution'){
 		checkout([$class: 'GitSCM',
@@ -15,10 +34,9 @@ node {
                  "clean test -Ppayment "
 	
 		publishHTML([reportDir: 'test-output', reportFiles: 'PaymentServiceReport.html', reportName: 'Payment Test-suite Report'])
-		
-		def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-		total = testResultAction.getTotalCount()
-		println(total)
+		step $class: 'JUnitResultArchiver', testResults: 'test-output/testng-results.xml'
+		testSummary = getTestSummary()
+		println(testSummary)
 	currentBuild.result = 'UNSTABLE'
 	}
 	/*stage('Email')

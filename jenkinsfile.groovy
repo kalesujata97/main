54c5e35d17b55e699f5d6540bdea9166a1d18507
Sujata Kale <49482604+SujataKale97@users.noreply.github.com>
2019-05-28 09:45:38 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -13,7 +13,8 @@ node {
 	
 		publishHTML([reportDir: 'test-output', reportFiles: 'PaymentServiceReport.html', reportName: 'Payment Test-suite Report'])
 		
-		
+		def result = build.getAction(hudson.tasks.junit.TestResultAction.class).result
+		println(result)
 	}
 /*	stage('Email')
         {
